216918df13bac9b6e53bab3394f7fff7069cba10
huytrinh <dinhhuyams@gmail.com>
2021-06-17 06:57:30 +0300

    chore: setup lint

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+function App() {
+    const heading = "JavaScript tooling"
+
+    return (
+        <>
+            <h1>{heading}</h1>
+            <p>Welcome to the presentation</p>
+        </>
+    )
+}
+
+export default App
